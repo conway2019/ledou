74e19f7c432a30b20d2f156b54e23c53f1710d5c
康威 <kangwei@xinhua.org>
2024-09-08 18:06:36 +0800

    'update'

---
+++ pages/text.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8
+import streamlit as st
+from openai import OpenAI
+import os
+import json
+import time
+import yaml
+
+# Create a chatbot UI with Streamlit and OpenAI
+def chat_ui():
+   # 打开并读取 YAML 文件
+    with open('../configs/config.yaml', 'r', encoding='UTF-8') as file:
+        config = yaml.safe_load(file)
+        # Get the max token length for the chatbot
+        max_tokens = config.get('max_tokens', 100)
+        # Get the temperature for the chatbot
+        temperature = config.get('temperature', 0.0)
+        # Get the api key for the OpenAI API
+        api_key = config.get('api_key', "internlm2")
+        # Get the base url for the OpenAI API
+        base_url = config.get('base_url', "http://0.0.0.0:23333/v1")
+        # Get the system prompt for the chatbot
+        system_prompt = config.get('system_prompt', "我是乐豆小助手，逗乐不停，欢乐满荧！")
+    
+        print(base_url)
+        llm = OpenAI(api_key=api_key, base_url=base_url)
+        print(llm)
+        print(llm.models.list().data[0].id)
+            
+    #隐藏streamlit菜单
+    st.set_page_config(menu_items=None)
+    
+    state = st.session_state
+    # Set the title of the app
+    st.title("乐豆：逗乐不停，欢乐满荧")
+    st.caption("你喜欢什么样的笑话？冷笑话、幽默笑话、职场笑话、儿童笑话？说出你的想法吧！")
+
+    message_history = []
+    user_input = st.chat_input("输入消息")
+    if user_input:
+        message_history.append({"role": "user", "content": user_input})
+        # Generate a response from the chatbot
+        response = llm.chat.completions.create(
+            model=llm.models.list().data[0].id,
+            messages=message_history,
+            max_tokens=max_tokens,
+            temperature=temperature
+        )
+
+        message_history.append({"role": "assistant", "content": response.choices[0].message.content})
+        pass
+    for message in message_history:
+        if message["role"] == "system":
+            continue
+        else:
+            st.chat_message(message["role"]).write(message["content"])
+
+    # Create a text input for the user to type their message
+
+    pass
+
+if __name__ == "__main__":
+    chat_ui()
+    pass
+
+    
